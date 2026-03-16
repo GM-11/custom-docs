0efae95fb29218c058c529ba4329180d2860a814
Gopal Mathur <mathuyrgopal@gmail.com>
2026-03-16 14:30:36 +0530

    using ot_ffi

--- ot-engine/ffi/wrappers.h
+++ ot-engine/ffi/wrappers.h
@@ -1,27 +1,4 @@
 #include "../include/operation.h"
-extern "C" {
-
-struct OperationC {
-  int type;
-  int position;
-  int version;
-  char *clientId;
-  union {
-    char *text;
-    int length;
-  } data;
-};
-
-OperationC *performTransformation(const OperationC *op1, const OperationC *op2,
-                                  int *outsize);
-char *applyTransformations(char *currentDocument, OperationC *latestOperation);
-OperationC *transformPipeLine(const OperationC *operations,
-                              const OperationC *incomingOperation, int *outsize,
-                              int *operationsSize);
-
-void freeOperations(OperationC *ops, int size);
-void freeDocument(char *doc);
-}
-
+#include "./ot_ffi.h"
 OperationC convertOperationToOperationC(const Operation op);
 Operation convertOperationCToOperation(const OperationC op);
