4622ccae1589f5d3fe70276cd90e69e31fc7d7b0
Gopal Mathur <mathuyrgopal@gmail.com>
2026-03-25 16:52:38 +0530

    Replace Operation with Payload in ChannelData

--- connection-manager/models/channel_data.go
+++ connection-manager/models/channel_data.go
@@ -1,9 +1,7 @@
 package models
 
-import "custom_docs.com/m/v2/engine"
-
 type ChannelData struct {
-	Operation engine.Operation
-	ClientId  string
-	Version   int
+	Payload  MessagePayload
+	ClientId string
+	Version  int
 }
