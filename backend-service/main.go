95404fc84d626e2b2df37d84e6614c22c055962f
Gopal Mathur <mathuyrgopal@gmail.com>
2026-03-25 16:08:31 +0530

    added http connection methods for managing documents

--- backend-service/main.go
+++ backend-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,33 +17,72 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func handleWebSocket(hub *models.Hub) http.HandlerFunc {
+func createNewDocumentHandler(manager *models.HubManager) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		clientId := r.URL.Query().Get("clientId")
+		if clientId == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{"error": "clientId is required"})
+			return
+		}
+		// conn, err := upgrader.Upgrade(w, r, nil)
+
+		hubId, err := manager.CreateNewDocument(clientId)
+		if err != nil {
+			log.Println("Error creating new document:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err)
+		}
+
+		json.NewEncoder(w).Encode(hubId)
+	}
+}
+
+func connectToDocumentHandler(manager *models.HubManager) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		clientId := r.URL.Query().Get("clientId")
+		hubId := r.URL.Query().Get("hubId")
+
+		if clientId == "" || hubId == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{"error": "clientId and hubId are required"})
+			return
+		}
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Println("Upgrade error:", err)
 			return
 		}
-
-		fmt.Println("Client connected!")
-
-		client := models.Client{}
-		client.WsConn = conn
-		client.OutboundChannel = make(chan models.ChannelData)
-		client.Hub = hub
-		client.ClientId = r.URL.Query().Get("clientId")
-
-		hub.Register <- &client
-
-		go client.WritePump()
-		client.ReadPump()
+		manager.ConnectToDocument(clientId, hubId, conn)
 	}
 }
 
+func getDocumentStateHandler(manager *models.HubManager) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		hubId := r.URL.Query().Get("hubId")
+		if hubId == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{"error": "hubId is required"})
+			return
+		}
+
+		state, err := manager.GetDocumentState(hubId)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+			return
+		}
+		json.NewEncoder(w).Encode(state)
+
+	}
+}
 func main() {
-	hub := models.Hub{}
-	go hub.Run()
-	http.HandleFunc("/ws", handleWebSocket(&hub))
+	mux := http.NewServeMux()
+	manager := models.HubManager{Hubs: make(map[string]*models.Hub)}
+	mux.HandleFunc("POST /documents", createNewDocumentHandler(&manager))
+	mux.HandleFunc("GET /ws", connectToDocumentHandler(&manager))
+	mux.HandleFunc("GET /document", getDocumentStateHandler(&manager))
+
 	fmt.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
