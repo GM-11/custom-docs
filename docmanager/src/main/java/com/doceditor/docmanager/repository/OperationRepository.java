ab8885bf1b4f0b54aa0f13526fa9bd6f9f42a4dc
Gopal Mathur <mathuyrgopal@gmail.com>
2026-03-28 17:12:45 +0530

    Fixed sql query

--- docmanager/src/main/java/com/doceditor/docmanager/repository/OperationRepository.java
+++ docmanager/src/main/java/com/doceditor/docmanager/repository/OperationRepository.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OperationRepository extends JpaRepository<Operations, String> {
-    Optional<Operations> findByOperationId(String operationId);
+    Optional<Operations> findById(String operationId);
 
     List<Operations> findByDocumentIdOrderByLamportClockAsc(String documentId);
 
-    @Query(value = "SELECT * FROM operations WHERE o.documentId = :documentId AND o.lamportClock > :lamportClock ORDER BY o.lamportClock ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM operations WHERE id = CAST(:documentId AS uuid) AND lamport_clock > :lamportClock ORDER BY lamport_clock ASC", nativeQuery = true)
     List<Operations> findOperationsSinceSnapshot(@Param("documentId") String documentId,
             @Param("lamportClock") Long lamportClock);
 }
