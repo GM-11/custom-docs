7dc3f5a11ba92c442f187e94f1eec57fbbdc5ea9
Gopal Mathur <mathuyrgopal@gmail.com>
2026-04-10 16:46:43 +0530

    Added get user id by email

--- authentication/src/main/java/com/doceditor/authentication/repositories/UserRepository.java
+++ authentication/src/main/java/com/doceditor/authentication/repositories/UserRepository.java
@@ -6,9 +6,14 @@ import java.util.UUID;
 import com.doceditor.authentication.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT id FROM users WHERE email = :email", nativeQuery = true)
+    Optional<String> findUserIdByEmail(@Param("email") String email);
 
 }
